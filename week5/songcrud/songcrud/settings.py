7a82468895af46d5c039ec411d2415f160ea47e0
oseghae.oaikhena <oseghae.oaikhena@networkinv.com>
2022-10-26 13:54:45 -0600

    remove secret

--- week5/songcrud/songcrud/settings.py
+++ week5/songcrud/songcrud/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(hyy$^l)26a$=%!b262schbyz6v#g(#l^0nlqgr*fmga14di(u'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
